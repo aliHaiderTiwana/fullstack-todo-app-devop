5fd1f7c5cdee0994ac42e00f82e425af12aba70b
[200~aliHaiderTiwana~ <bscs23f41@namal.edu.pk>
2026-05-20 03:52:29 +0500

    feat: added multi-stage automation layout structure inside pipeline directory

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,3 +1,3 @@
-    const API_URL = process.env.REACT_APP_API_URL || 'https://app-taskapp-backend-aliaider.azurewebsites.net';
+    const API_URL = process.env.REACT_APP_API_URL || '';
 
 export default API_URL;
